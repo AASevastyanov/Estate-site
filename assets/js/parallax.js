8fbe8a733f11c81a5b70b28bb8eb9831cb7e3ec4
AASevastyanov <144042421+AASevastyanov@users.noreply.github.com>
2025-03-08 00:30:17 +0300

    Update parallax.js

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,19 +1,10 @@
-let lastKnownScrollPosition = 0;
-let ticking = false;
-const parallax = document.querySelector(".parallax");
-
-function doParallax(scrollPos) {
-  parallax.style.backgroundPositionY = `${scrollPos * 0.3}px`;
-}
-
+let lastScroll = 0;
 window.addEventListener("scroll", () => {
-  lastKnownScrollPosition = window.scrollY;
+  const scrollPosition = window.scrollY;
 
-  if (!ticking) {
-    window.requestAnimationFrame(() => {
-      doParallax(lastKnownScrollPosition);
-      ticking = false;
-    });
-
-    ticking = true;
+  requestAnimationFrame(() => {
+    document.querySelector(".parallax").style.backgroundPositionY =
+      -(scrollPosition * 0.3) + "px";
+  });
+  lastScroll = scrollPosition;
 });
